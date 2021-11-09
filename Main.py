f340d83293b68c30a8bc64ef5c45b9762661bd5a
CoinCode99 <88600594+COVoid99@users.noreply.github.com>
2021-11-09 14:20:48 -0800

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+import random
+
+print("type  'roast me'  and it will roast you     :)  ")
+print("")
+rm = input("")
+print("")
+list = ["boi if you don't shut yo filthy Goodyear blimp ass chicken leg ass fat dunked elephant head ass disowned child homless piece of shit the fuck up out of here", 
+"boi get yo ugly scared fat bed breakin small mouse-teeth fuckin burnt barbeque pied ass out of here", "boiiii if u dont get yo skinney bones fucking blacklobster stupid mcdonalds eating fat bob slave owning peter griffin cousin the fuck up out of here", 
+"bro shut yo god damn slow punky ass conspiracy theorist head ass fuckin ronald mcdonald fan headass built like peter griffin if he bought the whole burger king HEAD ASS", 
+"Boi if you don’t shut yo skin tone chicken bone google chrome no home flip phone disowned ice cream cone garden gnome extra chromosome metronome  student loan Indiana Jones overgrown flint stone x and y hormone friend zoned “leave me alone” head ass tf up out of here"]
+
+while True:
+ if rm == "roast me":
+    print(random.choices(list))
+    print("")
+    input("")
+    
+    
